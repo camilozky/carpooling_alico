449b80b6f20ffea158da79c8a380d34dae39b061
camilozky <camilo.iush@gmail.com>
2020-06-07 21:27:03 -0500

    activation of navigation links, refactor Function setActive and extract file nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,14 +10,7 @@
 	</style>
 </head>
 <body>
-	<nav>
-		<ul>
-			<li class="{{request()->routeIs('home') ? 'active' : ''}}"><a href="/">Home</a></li>
-			<li class="{{request()->routeIs('trips') ? 'active' : ''}}"><a href="/trips">List of Trips</a></li>
-			<li class="{{request()->routeIs('maketrip') ? 'active' : ''}}"><a href="/maketrip">Make a Trip</a></li>
-			<li class="{{request()->routeIs('jointrip') ? 'active' : ''}}"><a href="/jointrip">Join a Trip</a></li>
-		</ul>
-	</nav>
+	@include('partials.nav')
 	@yield('content')
 </body>
 </html>
