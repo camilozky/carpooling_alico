fac05d289c53c24c4cdeebb0bc0d77c90db101db
camilozky <camilo.iush@gmail.com>
2020-07-09 23:18:45 -0500

    Revert "route_resource_07_09_2020_23_08"
    This reverts commit a825d3e941dad408642995c62d0311a59d9f8aae.

--- routes/web.php
+++ routes/web.php
@@ -72,27 +72,24 @@ use Illuminate\Support\Facades\Route;
 App::setlocale('es');
 
 Route::view('/', 'home')->name('home');
-
-Route::resource('viajes','TripController')->names('trips');
-
-// Route::get('/viajes/crear', 'TripController@create')->name('trips.create');
-// Route::post('/viajes', 'TripController@store')->name('trips.store');
-
+Route::get('/viajes/crear', 'TripController@create')->name('trips.create');
+Route::post('/viajes', 'TripController@store')->name('trips.store');
 // Route::view('/trips', 'trips', compact('trips'))->name('trips');
 
-// Route::get('/viajes', 'TripController@index')->name('trips.index');
-// Route::get('/viajes/{trip}', 'TripController@show')->name('trips.show');
+Route::get('/viajes', 'TripController@index')->name('trips.index');
+Route::get('/viajes/{trip}', 'TripController@show')->name('trips.show');
 
-// Route::get('/viajes/{trip}/editar', 'TripController@edit')->name('trips.edit');
-// Route::patch('/viajes/{trip}', 'TripController@update')->name('trips.update');
+Route::get('/viajes/{trip}/editar', 'TripController@edit')->name('trips.edit');
+Route::patch('/viajes/{trip}', 'TripController@update')->name('trips.update');
 
-Route::get('/viajes/{trip}/sign', 'TripController@sign')->name('trips.sign');
-Route::patch('/viajes/{trip}/join', 'TripController@join')->name('trips.join');
+Route::get('/viajes/{trip}/inscrirse', 'TripController@sign')->name('trips.sign');
+Route::patch('/viajes/{trip}/unirse', 'TripController@join')->name('trips.join');
 
-// Route::delete('/viajes/{trip}', 'TripController@destroy')->name('trips.destroy');
+Route::delete('/viajes/{trip}', 'TripController@destroy')->name('trips.destroy');
 
 
 Route::view('/realizar-un-viaje', 'maketrip')->name('maketrip');
 Route::view('/unirse-a-un-viaje', 'jointrip')->name('jointrip');
 
 Route::get('/phpfirebase_sdk','FirebaseController@index');
+// Route::resource('trips','TripController');
