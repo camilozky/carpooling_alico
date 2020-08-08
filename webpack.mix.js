fa66da8d8dda67dee229ccaa81c0c3178b83be1a
camilozky <camilo.iush@gmail.com>
2020-08-08 17:39:37 -0500

    laravel_Mix_08082020

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,13 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.browserSync('http://carpooling_alico.test')
+
+
+
+if (mix.inProduction())
+{
+	mix.version();
+}
